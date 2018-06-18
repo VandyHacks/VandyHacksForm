57fd9578dbf89b278fa62c233d441e8554e33a34
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-06-18 17:38:56 -0400

    Update

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const PORT = process.env.PORT || 63650
+const PORT = process.env.PORT || 5000
 const parser = require('body-parser');
+const uri = 'mongodb://heroku_9d4txdmb:ol8lo56i5qd1u3ro3ubi7e3tug@ds163650.mlab.com:63650/heroku_9d4txdmb';
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect(uri);
+
+var hackerSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String
+})
+var User = mongoose.model("Participants", hackerSchema);
+
 
 express() 
   .set('view engine','ejs')
@@ -25,12 +37,3 @@ express()
   .listen(PORT, () => {
     console.log("Server listening on port " + PORT);
   })
-
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://heroku_9d4txdmb:ol8lo56i5qd1u3ro3ubi7e3tug@ds163650.mlab.com:63650/heroku_9d4txdmb");
-
-var nameSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String
-})
-var User = mongoose.model("User", nameSchema);
