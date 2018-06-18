64c13af5f50b1591b87882b6b9e58db2fa1a8731
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-06-18 18:58:16 -0400

    Update

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const PORT = process.env.PORT || 5000
 const parser = require('body-parser');
 const uri = 'mongodb://heroku_9d4txdmb:ol8lo56i5qd1u3ro3ubi7e3tug@ds163650.mlab.com:63650/heroku_9d4txdmb';
 
-var app = express();
-app.use(parser.json());
-app.use(parser.urlencoded({ extended: true }));
 
 mongoose.Promise = global.Promise;
 mongoose.connect(uri);
@@ -17,21 +15,25 @@ var hackerSchema = new mongoose.Schema({
 })
 var User = mongoose.model("Participants", hackerSchema);
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/form.html");
-});
 
-app.post("/add", (req, res) => {
-  var data = new User(req.body);
-  data.save()
-    .then(item => {
-      res.send("Saved to database");
-    })
-    .catch(err => {
-      res.status(400).send("Unable to save to database");
-    });
-});
-
-app.listen(PORT, () => {
-  console.log("Server listening on port " + port);
-});
+express() 
+  .set('view engine','ejs')
+  .use('/', (req, res) => {
+    res.sendFile(__dirname + "/form.html");
+  })
+  .get('/', (req, res) => res.render('pages/index'))
+  .post('/add', (req, res) => {
+      var data = new User(req.body);
+      data.save()
+        .then(item => {
+          res.send("item saved to database");
+        })
+        .catch(err => {
+          res.status(400).send("unable to save to database");
+        });
+  })
+  .use(parser.json())
+  .use(parser.urlencoded({ extended: true }))
+  .listen(PORT, () => {
+    console.log("Server listening on port " + PORT);
+  })
