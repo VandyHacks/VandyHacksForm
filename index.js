eed14d3d235b40ad10ade1b5e3d21c0c8f113e1b
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-06-18 14:17:18 -0400

    Implement Mongo

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const PORT = process.env.PORT || 5000
-
+const parser = require('body-parser');
 
 express() 
   .set('view engine','ejs')
@@ -9,14 +10,27 @@ express()
     res.sendFile(__dirname + "/form.html");
   })
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM table');
-      res.render('pages/db', result);
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
+  .post("/add", (req, res) => {
+      var data = new User(req.body);
+      data.save()
+        .then(item => {
+          res.send("item saved to database");
+        })
+        .catch(err => {
+          res.status(400).send("unable to save to database");
+        });
   })
+  .use(parser.json())
+  .use(parser.urlencoded({ extended: true }))
+  .listen(PORT, () => {
+    console.log("Server listening on port " + PORT);
+  })
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://kzhai:vandyhacks5@ds251819.mlab.com:51819/vh-walk-in-form");
+
+var nameSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String
+})
+var User = mongoose.model("User", nameSchema);
