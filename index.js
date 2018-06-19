5439ab8ddb4f079a01de2856ee189d1280d5c81b
k-zhai <32788271+k-zhai@users.noreply.github.com>
2018-06-19 00:35:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,36 @@
-const {Pool} = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const PORT = process.env.PORT || 5000
+const parser = require('body-parser');
+const uri = 'mongodb://heroku_9d4txdmb:ol8lo56i5qd1u3ro3ubi7e3tug@ds163650.mlab.com:63650/heroku_9d4txdmb';
+
+mongoose.Promise = global.Promise;
+mongoose.connect(uri);
+
+var hackerSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  phone: String
+})
+var Hacker = mongoose.model("Participants", hackerSchema);
+
+var app = express();
+app.use('/', (req, res) => {
+  res.sendFile(__dirname + "/form.html");
+});
+app.post('/add', (req, res) => {
+  var data = new User(req.body);
+  data.save()
+    .then(item => {
+      res.send("Saved to database");
+    })
+    .catch(err => {
+      res.status(400).send("Unable to save to database");
+    });
+})
+app.listen(PORT, () => {
+  console.log("Server listening on port " + PORT);
 });
 
-express() 
-  .set('view engine','ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM table');
-      res.render('pages/db', result);
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-  .post('/person', function (req, res) {
-    var info = req.body;
-    if (!info.name || !info.preference || !info.email || !info.phone) {
-      res.render('show_message', {
-        message: "Invalid input", type: "error" });
-    } else {
-      var newPerson = new Person ({
-        name = info.name,
-        email = info.email,
-        phone = info.phone,
-        preference = info.preference
-      });
-      newPerson.save(function(err, Person) {
-        if (err)
-          res.render('show_message', {message: "Database error", type: "error"});
-        else 
-          res.render('show_message', {
-            message: "New person added", type: "success", person: info
-          });
-      })
-    }
-  })
