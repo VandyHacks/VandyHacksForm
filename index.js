eaa79d48782fae265d2297449327e5dfab5de220
k-zhai <32788271+k-zhai@users.noreply.github.com>
2018-06-24 21:02:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
 const parser = require('body-parser');
 const uri = 'mongodb://vhdev:vandyhacks5@ds163650.mlab.com:63650/heroku_9d4txdmb';
+const { check, validationResult } = require('express-validator/check');
+const { matchedData, sanitize } = require('express-validator/filter');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -14,29 +15,44 @@ mongoose.Promise = global.Promise;
 
 var db = mongoose.connection;
 
-app.use('/', (req, res) => {
-  res.sendFile(__dirname + "/form.html");
-})
-
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback() {
-  var hackerSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    phone: String
-  })
-  var Hacker = mongoose.model("Hacker", hackerSchema);
-  app.post('/add', (req, res) => {
-    var data = new Hacker(req.body);
-    data.save()
-      .then(item => {
-        res.send("Saved to database");
-      })
-      .catch(err => {
-        res.send("Unable to save to database");
-      })
-  }) 
+  console.log("Database open");
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/form.html");
+  console.log("Page loaded");
+})
+
+var hackerSchema = new mongoose.Schema({
+  firstName: {type: String, max: 20},
+  lastName: {type: String, max: 20},
+  email: {type: String, max: 100},
+  phone: {type: String, max: 15}
+})
+var Hacker = db.model("Hacker", hackerSchema);
+
+app.post('/', [
+  check('firstName', 'Enter valid name')
+    .isAlpha(),
+  check('lastName', 'Enter valid name')
+    .isAlpha(),
+  check('email', 'Enter valid email')
+    .isEmail()
+    .normalizeEmail(),
+  check('phone', 'Enter valid phone number')
+    .isMobilePhone()
+], (req, res) => {
+  var data = new Hacker(req.body);
+  data.save()
+    .then(item => {
+      res.send("Saved to database");
+      console.log("Added one entry");
+    })
+    .catch(err => {
+      res.send("Unable to save to database");
+    })
 })
 
 app.listen(PORT, () => {
