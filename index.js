ff49578466080136f4a46b438210bbf84aa7d34b
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2018-07-06 23:24:49 -0400

    Update

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
 const parser = require('body-parser');
 const uri = 'mongodb://vhdev:vandyhacks5@ds163650.mlab.com:63650/heroku_9d4txdmb';
+const { check, validationResult } = require('express-validator/check');
+const { matchedData, sanitize } = require('express-validator/filter');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(parser.urlencoded({ extended: true }))
 app.use(parser.json())
+
+app.use(helmet());
 
 mongoose.connect(uri);
 mongoose.Promise = global.Promise;
@@ -31,7 +36,17 @@ var hackerSchema = new mongoose.Schema({
 })
 var Hacker = db.model("Hacker", hackerSchema);
 
-app.post('/', (req, res) => {
+app.post('../submitted.html', [
+  check('firstName', 'Enter valid name')
+    .isAlpha(),
+  check('lastName', 'Enter valid name')
+    .isAlpha(),
+  check('email', 'Enter valid email')
+    .isEmail()
+    .normalizeEmail(),
+  check('phone', 'Enter valid phone number')
+    .isMobilePhone()
+], (req, res) => {
   var data = new Hacker(req.body);
   data.save()
     .then(item => {
