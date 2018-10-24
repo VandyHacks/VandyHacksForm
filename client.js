77a91c3c54b6dce5515f7c9332cd68a6b17fc8a8
Jess0-0 <jiayao.wang@vanderbilt.edu>
2018-10-23 20:00:21 -0500

    add to submitForm

--- client.js
+++ client.js
@@ -56,8 +56,26 @@ $("#school").on('keypress', e => {
 
 
 function submitform() {
-  // TODO: fix form submit
-  alert('form submitted.')
+  var XHR = new XMLHttpRequest();
+  var FD  = new FormData(document.getElementById("myForm"));
+
+  // Define what happens on successful data submission
+  XHR.addEventListener("load", function(event) {
+    alert('Form Submitted!');
+  });
+
+  // Define what happens in case of error
+  XHR.addEventListener("error", function(event) {
+    alert('Oops! Something went wrong.');
+  });
+
+  // Set up our request
+  XHR.open('POST', 'https://apply.vandyhacks.org/api/walkin/profile');
+
+  XHR.setRequestHeader(header, 'x-event-secret');
+
+  // Send our FormData object; HTTP headers are set automatically
+  XHR.send(FD);
   return false;
 };
 
