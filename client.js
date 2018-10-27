c9b7749669919eac6c3fdff1ad36f61e2f12ef8a
Melvin Lu <melvinlu1996@gmail.com>
2018-10-27 18:12:48 -0500

    Fix cors proxy

--- client.js
+++ client.js
@@ -1,18 +1,18 @@
-const dataList = document.getElementById('json-datalist');
-const input = document.getElementById('school');
+const dataList = document.getElementById("json-datalist");
+const input = document.getElementById("school");
 const universityList = UNIVERSITIES;
 let token;
 
 const COLORS = {
-  GREEN: 'rgb(80, 187, 80)',
-  RED: 'rgb(139, 0, 0)',
-  LIGHT: 'rgb(230, 230, 230)',
-  DARK: 'rgb(60, 56, 80)'
-}
+  GREEN: "rgb(80, 187, 80)",
+  RED: "rgb(139, 0, 0)",
+  LIGHT: "rgb(230, 230, 230)",
+  DARK: "rgb(60, 56, 80)"
+};
 
 window.onload = event => {
   // fill grad year select
-  ['2018', '2019', '2020', '2021', '2022'].forEach(e => {
+  ["2018", "2019", "2020", "2021", "2022"].forEach(e => {
     const newoption = dom("<option>");
     newoption.value = e;
     newoption.text = e;
@@ -20,7 +20,7 @@ window.onload = event => {
   });
 
   // fill gender select
-  ['M', 'F', 'O', 'N'].forEach(e => {
+  ["M", "F", "O", "N"].forEach(e => {
     const newoption = dom("<option>");
     newoption.value = e;
     newoption.text = e;
@@ -32,60 +32,58 @@ window.onload = event => {
 function updateUnivList() {
   // TODO: fix dropdown is only shown properly when pressing backspace, doesn't show when typing
   // clear
-  dataList.innerHTML = '';
+  dataList.innerHTML = "";
   // match input
   const inputText = input.value.trim();
   const criteria = e => e.toLowerCase().includes(inputText.toLowerCase());
   const results = universityList.filter(criteria).slice(0, 5);
 
   // build new list
-  results.forEach((item) => {
-    const option = document.createElement('option');
+  results.forEach(item => {
+    const option = document.createElement("option");
     option.value = item;
     // check if the item already exists in dataList; remove an item if length > 4
     dataList.appendChild(option);
   });
-  console.log(dataList.options)
+  console.log(dataList.options);
 }
 
 dom("#school").addEventListener("keyup", e => {
-  console.log(e.keyCode)
+  console.log(e.keyCode);
   if (e.keyCode === 13) {
-    console.log('Enter pressed.')
+    console.log("Enter pressed.");
     // put top result of dropdown as html value
-    input.value = dataList.options[0].value; 
+    input.value = dataList.options[0].value;
     // focus on next elem
-    dom('#email').focus();
+    dom("#email").focus();
   }
   updateUnivList();
 });
 
-
 function submitform(e) {
   e.preventDefault();
-  fetch(transformURL('https://apply.vandyhacks.org/api/walkin/profile'), 
-  {  
-    method: 'POST',  
+  fetch(transformURL("https://apply.vandyhacks.org/api/walkin/profile"), {
+    method: "POST",
     headers: new Headers({
-      "x-event-secret": token,
+      "x-event-secret": token
     }),
     body: JSON.stringify({
-      name: dom('#name').value,
-      school: dom('#school').value,
-      email: dom('#email').value,
-      phone: dom('#phone').value,
-      year: dom('#year').value,
-      gender: dom('#gender').value,
+      name: dom("#name").value,
+      school: dom("#school").value,
+      email: dom("#email").value,
+      phone: dom("#phone").value,
+      year: dom("#year").value,
+      gender: dom("#gender").value
     })
   })
-  .then(data => {  
-    console.log('Request success: ', data);
-  })  
-  .catch(error => {  
-    console.log('Request failure: ', error);
-  });
+    .then(data => {
+      console.log("Request success: ", data);
+    })
+    .catch(error => {
+      console.log("Request failure: ", error);
+    });
   return false;
-};
+}
 
 /**************************************************************************************************/
 /*********************************** Authorization stuff ******************************************/
@@ -135,20 +133,19 @@ dom("#authcode").addEventListener("keyup", e => {
   }
 });
 
-
 /**************************************************************************************************/
 /****************************************** Utils *************************************************/
 
 function transformURL(url) {
   const isDev = !location.hostname.endsWith("vandyhacks.org");
-  // bypass CORS issues in client-side API calls during localhost/dev, see https://github.com/Freeboard/thingproxy
-  return isDev ? "https://thingproxy.freeboard.io/fetch/" + url : url;
+  // bypass CORS issues in client-side API calls during localhost/dev
+  return isDev ? "https://cors-anywhere.herokuapp.com/" + url : url;
 }
 
 // toggle hiding elems
 function hideInputs(hide) {
   const elems = ["#forms"];
   elems.forEach(e => {
-    dom(e).style.display = hide ? 'none' : 'block';
+    dom(e).style.display = hide ? "none" : "block";
   });
 }
